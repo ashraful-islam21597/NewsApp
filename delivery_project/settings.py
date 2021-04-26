5a35874223400422bf195412adef7e32f43319a3
ashraful-islam21597 <ashraful.97mdp@gmail.com>
2021-04-26 17:30:32 +0600

    edit

--- delivery_project/settings.py
+++ delivery_project/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 django_heroku.settings(locals())
 

da61034fd51b9281c28e55bc349729942b46a6bf
ashraful-islam21597 <ashraful.97mdp@gmail.com>
2021-04-26 17:13:54 +0600

    edit

--- delivery_project/settings.py
+++ delivery_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n^38$$=hc3zh2ogt%0%b!3g2z#c@igkm=7r7+sdkyc0ote&hi2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
